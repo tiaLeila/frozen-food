c943e5dd19eb0801e7fa84ea9fffca31400e25f4
tiaLeila <tialeilarefeicoes@gmail.com>
2020-03-24 14:49:29 -0300

    Updates

---
+++ precache-manifest.75e7d8ddad6abcc6ce528c6be5e0f5d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc091c4ce97ba60eef29d6828c98121c",
+    "url": "/frozen-foods/index.html"
+  },
+  {
+    "revision": "b0b55553d0a0521f34c5",
+    "url": "/frozen-foods/static/css/main.4a4b0664.chunk.css"
+  },
+  {
+    "revision": "8cf67ce0cdc613761065",
+    "url": "/frozen-foods/static/js/2.f0e9025a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frozen-foods/static/js/2.f0e9025a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b55553d0a0521f34c5",
+    "url": "/frozen-foods/static/js/main.3884d168.chunk.js"
+  },
+  {
+    "revision": "5193e038d278d28c4b15",
+    "url": "/frozen-foods/static/js/runtime-main.310e6d8c.js"
+  },
+  {
+    "revision": "481c13ecb1f12203a4f5a9ed1f3d1906",
+    "url": "/frozen-foods/static/media/logoSlogan.481c13ec.png"
+  }
+]);
